737986e33c5e72e505d3b195c78d7113710ba9a9
Katarzyna Lesiak <katarzyna.lesiak@medius.com>
2022-02-15 11:21:38 +0100

    34-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 55
+let secondNumber = 77
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(sum (firstNumber, secondNumber));
